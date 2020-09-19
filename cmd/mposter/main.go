b8505eac0658d927d52be304a045714683375bcb
Mykola Gurov <ngurov@gmail.com>
2020-09-19 12:30:55 +0200

    refactored the main loop
    to have explicitly separate line url processors
    
    fixes #33

--- cmd/mposter/main.go
+++ cmd/mposter/main.go
@@ -29,16 +29,55 @@ func main() {
 
 func run(params runparams.RunParams) error {
 
-	scanner := bufio.NewScanner(params.Input)
+	paramsToUrl, err := makeParamsToUrlFun(params)
+	if err != nil {
+		return err
+	}
 
+	lineUrlProcessor, onProcessingDone := makeLineUrlProcessor(params)
+	defer onProcessingDone() //TODO: test this is invoked
+
+	skipLines := params.Skip
+
+	scanner := bufio.NewScanner(params.Input)
+	for scanner.Scan() {
+		nextLine := strings.TrimSpace(scanner.Text())
+
+		if skipLines > 0 {
+			skipLines--
+			continue
+		}
+
+		//TODO: this one will probably interfere with the skip lines feature.
+		if nextLine == "" {
+			continue
+		}
+		fmt.Fprint(params.Output, nextLine, " ")
+		urlToCall, err := paramsToUrl(nextLine)
+		if err != nil {
+			return err
+		}
+
+		err = lineUrlProcessor(urlToCall)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type ParamsToUrlFun func(params string) (string, error)
+
+func makeParamsToUrlFun(params runparams.RunParams) (paramsToUrl ParamsToUrlFun, err error) {
 	templated := strings.Contains(params.Url, "{{")
 
-	var paramsToUrl func(string) (string, error)
 	if templated {
 
 		f, err := urltemplate.Parse(params.Url)
 		if nil != err {
-			return fmt.Errorf("parse url template \"%s\": %w", params.Url, err)
+			return nil, fmt.Errorf("parse url template \"%s\": %w", params.Url, err)
 		}
 
 		paramsToUrl = func(line string) (string, error) {
@@ -60,6 +99,21 @@ func run(params runparams.RunParams) error {
 		}
 	}
 
+	return
+
+}
+
+type LineUrlProcessor func(urlToCall string) error
+type LineUrlProcessingDone func()
+
+func makeLineUrlProcessor(params runparams.RunParams) (LineUrlProcessor, LineUrlProcessingDone) {
+	if params.DryRun {
+		return func(urlToCall string) error {
+			fmt.Fprintln(params.Output, params.HttpMethod, urlToCall)
+			return nil
+		}, func() {}
+	}
+
 	tracker := tracker.Tracker{
 		StopOnFirstErr:            params.StopOnFirstError,
 		StopOnConsecutiveErrCount: params.StopOnErrorCount,
@@ -70,80 +124,17 @@ func run(params runparams.RunParams) error {
 		tracker.Logger = log.New(os.Stderr, "", log.LstdFlags)
 	}
 
-	defer func() {
-		tracker.LogDone()
-	}()
-
 	httpClient := http.Client{
 		Timeout: params.Timeout,
 	}
 
-	skipLines := params.Skip
-
-	for scanner.Scan() {
-		nextLine := strings.TrimSpace(scanner.Text())
-
-		if skipLines > 0 {
-			skipLines--
-			continue
-		}
-
-		//TODO: this one will probably interfere with the skip lines feature.
-		if nextLine == "" {
-			continue
-		}
-		fmt.Fprint(params.Output, nextLine, " ")
-		urlToCall, err := paramsToUrl(nextLine)
-		if err != nil {
-			return err
-		}
-
-		if params.DryRun {
-			fmt.Fprintln(params.Output, params.HttpMethod, urlToCall)
-			continue
-		}
-
-		req, err := http.NewRequest(params.HttpMethod, urlToCall, nil)
-		if err != nil {
-			return fmt.Errorf("Unexpected error creating request to %s : %w", urlToCall, err)
-		}
-		if params.HttpAcceptType != "" {
-			req.Header.Add("Accept", params.HttpAcceptType)
-		}
-		if params.HttpContentType != "" {
-			req.Header.Add("Content", params.HttpContentType)
-		}
-
-		resp, err := httpClient.Do(req)
-		if err != nil {
-			if urlErr, ok := err.(*url.Error); ok {
-				if urlErr.Timeout() {
-					fmt.Fprintln(params.Output, "ERR Timeout")
-				} else {
-					fmt.Fprintln(params.Output, "ERR", urlErr)
-				}
-				if bailoutErr := tracker.Err(); bailoutErr != nil {
-					return bailoutErr
-				}
-			} else {
-				return fmt.Errorf("Unexpected error posting to %s : %w", urlToCall, err)
-			}
-		} else {
-			defer resp.Body.Close()
-
-			if resp.StatusCode/100 == 2 {
-				fmt.Fprintf(params.Output, "OK\n")
-				tracker.Ok()
-			} else {
-				fmt.Fprintln(params.Output, "ERR HTTP", resp.StatusCode)
-				if bailoutErr := tracker.Err(); bailoutErr != nil {
-					return bailoutErr
-				}
-			}
-		}
+	caller := HttpCaller{
+		Tracker:    &tracker,
+		HttpClient: &httpClient,
+		Params:     params,
 	}
 
-	return nil
+	return caller.Call, tracker.LogDone
 }
 
 func splitRows(input, fieldSeparators string) []string {
@@ -154,4 +145,58 @@ func splitRows(input, fieldSeparators string) []string {
 	return strings.FieldsFunc(input, func(it rune) bool {
 		return unicode.IsSpace(it) || strings.ContainsRune(fieldSeparators, it)
 	})
+}
+
+type HttpCaller struct {
+	//ParamsToUrl func(string) (string, error)
+	Tracker    *tracker.Tracker
+	HttpClient *http.Client
+	Params     runparams.RunParams
+}
+
+func (c HttpCaller) Call(urlToCall string) error {
+	req, err := http.NewRequest(c.Params.HttpMethod, urlToCall, nil)
+	if err != nil {
+		return fmt.Errorf("Unexpected error creating request to %s : %w", urlToCall, err)
+	}
+	if c.Params.HttpAcceptType != "" {
+		req.Header.Add("Accept", c.Params.HttpAcceptType)
+	}
+	if c.Params.HttpContentType != "" {
+		req.Header.Add("Content", c.Params.HttpContentType)
+	}
+
+	resp, err := c.HttpClient.Do(req)
+
+	if err != nil {
+		if urlErr, ok := err.(*url.Error); ok {
+			if urlErr.Timeout() {
+				fmt.Fprintln(c.Params.Output, "ERR Timeout")
+			} else {
+				fmt.Fprintln(c.Params.Output, "ERR", urlErr)
+			}
+
+			if bailoutErr := c.Tracker.Err(); bailoutErr != nil {
+				return bailoutErr
+			} else {
+				return nil //swallow the error
+			}
+
+		} else {
+			return fmt.Errorf("Unexpected error posting to %s : %w", urlToCall, err)
+		}
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 == 2 {
+		fmt.Fprintf(c.Params.Output, "OK\n")
+		c.Tracker.Ok()
+	} else {
+		fmt.Fprintln(c.Params.Output, "ERR HTTP", resp.StatusCode)
+		if bailoutErr := c.Tracker.Err(); bailoutErr != nil {
+			return bailoutErr
+		}
+	}
+	return nil
 }
